a327410de95a45ef8117dbc303ee93eab1efef29
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-09 12:31:48 +0200

    JSX

--- React/index.js
+++ React/index.js
@@ -73,3 +73,16 @@ const h1 = document.createElement("h1")
 h1.textContent = "This is an imperative way to program"
 h1.className = "header"
 document.getElementById("root-swp").append(h1)
+
+
+// JSX
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+ReactDOM.render(
+    page,
+    document.getElementById("root-swp2")
+)
