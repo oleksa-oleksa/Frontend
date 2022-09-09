3ebfea76a478f33d0c354906b574f6aeb74bfd42
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-09 12:25:34 +0200

    Vanila JS example

--- React/index.js
+++ React/index.js
@@ -57,3 +57,19 @@ ReactDOM.render(
     </div>,
     document.getElementById("root")
 )
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root-swp").append(h1)
