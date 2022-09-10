ad03a570ce8bdbc29eede4df1af777f5b2d416aa
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-10 07:42:41 +0200

    folder reorganisation and first react app with npm

--- React/first-react/src/index.js
+++ React/first-react/src/index.js
@@ -12,5 +12,19 @@ const navbar = (
     </nav>
 )
 
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
 // ReactDOM.render(navbar, document.getElementById("root"))
 ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+ReactDOM.createRoot(document.getElementById("root-2")).render(page)
