0f59f10077fc6bdb8b15bec9f5baecc9d62e1129
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-10 08:09:05 +0200

    rendered nested structure

--- React/first-react/src/index.js
+++ React/first-react/src/index.js
@@ -1,19 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const navbar = (
-    <nav>
-        <h1>Oleksandra</h1>
-        <ul>
-            <li>Machine Learning</li>
-            <li>Frontend</li>
-            <li>Embedded Systems</li>
-        </ul>
-    </nav>
-)
-
 const page = (
     <div>
+        <nav>
+            <h1>Oleksandra</h1>
+            <ul>
+                <li>Machine Learning</li>
+                <li>Frontend Programming</li>
+                <li>Embedded Systems</li>
+            </ul>
+        </nav>
         <h1>My awesome website in React</h1>
         <h3>Reasons I love React</h3>
         <ol>
@@ -25,6 +22,7 @@ const page = (
     </div>
 )
 
+
+console.log(page)
 // ReactDOM.render(navbar, document.getElementById("root"))
-ReactDOM.createRoot(document.getElementById("root")).render(navbar)
-ReactDOM.createRoot(document.getElementById("root-2")).render(page)
+ReactDOM.createRoot(document.getElementById("root")).render(page)
