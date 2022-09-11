da3f27c287c470c16e9c7eab2ffad5207abe98f2
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-11 14:24:10 +0200

    02 project created structure

---
+++ React/02_react_project/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <h1>
+            This is a main
+            </h1>
+    )
+}
---
+++ React/02_react_project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from '../components/Navbar'
+import Main from '../components/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
