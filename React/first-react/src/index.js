d714cc3844a42727d0f77fc1b5cf45b46cc28b9b
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-11 08:40:21 +0200

    added styles

--- React/first-react/src/index.js
+++ React/first-react/src/index.js
@@ -1,27 +1,63 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-function CreateContent() {
-    <div>
-        <nav>
-            <h1>Oleksandra</h1>
-            <ul>
-                <li>Machine Learning</li>
-                <li>Frontend Programming</li>
-                <li>Embedded Systems</li>
-            </ul>
-        </nav>
-        <h1>My awesome website in React</h1>
-        <h3>Reasons I love React</h3>
-        <ol>
-            <li>It's composable</li>
-            <li>It's declarative</li>
-            <li>It's a hireable skill</li>
-            <li>It's actively maintained by skilled people</li>
-        </ol>
-    </div>
+function Header() {
+    return(
+        <div>
+            <header>
+                <nav className="nav">
+                    <logo>
+                        <img src="logo192.png" width="40px" alt="Logo"/>
+                    </logo>
+                    <ul className="nav-items">
+                        <li>Pricing</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
 }
 
-console.log(<CreateContent />)
+function Footer() {
+    return (
+        <small>Oleksandra Baga learns React, 2022</small>
+    )
+}
+
+function Content() {
+    return (
+        <div>
+            <nav>
+                <h1>Oleksandra</h1>
+                <ul>
+                    <li>Machine Learning</li>
+                    <li>Frontend Programming</li>
+                    <li>Embedded Systems</li>
+                </ul>
+             </nav>
+            <h1>My awesome website in React</h1>
+            <h3>Reasons I love React</h3>
+            <ol>
+                <li>It's composable</li>
+                <li>It's declarative</li>
+                <li>It's a hireable skill</li>
+                <li>It's actively maintained by skilled people</li>
+            </ol>
+        </div>
+    )
+}
+
+function Page() {
+    return(
+        <div>
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    )
+}
+
 // ReactDOM.render(navbar, document.getElementById("root"))
-ReactDOM.createRoot(document.getElementById("root")).render(<CreateContent />)
+ReactDOM.createRoot(document.getElementById("root")).render(<Page />)
