256dd45f4699d8b3641c977f402ecdc1dc7f4236
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-13 09:28:15 +0200

    create badge with react and props

--- React/Airbnb_clone/components/Card.jsx
+++ React/Airbnb_clone/components/Card.jsx
@@ -1,9 +1,24 @@
 import React from "react"
 
-export default function Card() {
+export default function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <section className="card">
-            <img className="card--photo" src="/katie-zaferes.png" alt="Kathie" />
+            {badgeText && <div className="card--badge">{badgeText}}</div>}
+            <img className="card--photo" src={`./${props.img}`} alt="Experience picture" />
+            <div className="card--stats">
+                <img className="card--star"  src="../src/assets/star.png" alt="star" />
+                <span>{props.rating}</span>
+                <span className="card--country">({props.reviewCount}) • </span>
+                <span className="card--country">{props.location}</span>
+            </div>
+            <p>{props.title}</p>
+            <p><span className="card--price">From €{props.price}</span> / person</p>
 
         </section>
     )
--- React/Airbnb_clone/src/App.jsx
+++ React/Airbnb_clone/src/App.jsx
@@ -2,13 +2,32 @@ import Navbar from '../components/Navbar'
 import Main from '../components/Main'
 import Hero from '../components/Hero'
 import Card from '../components/Card'
+import data from './data'
 
 function App() {
+
+  const cardData = data.map(card => {
+    return (
+      <Card 
+      key={card.id}
+      img={card.coverImg}
+      rating={card.stats.rating}
+      reviewCount={card.stats.reviewCount}
+      location={card.location}
+      title={card.title}
+      price={card.price}
+      openSpots={card.openSpots}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <section className="cards-list">
+        {cardData}
+      </section>
     </div>
   )
 }
