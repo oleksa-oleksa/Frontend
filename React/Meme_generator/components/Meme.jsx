3d1bfe140f9e0a72cdfd92dfe26c1587a04934a9
Oleksandra Baga <oleksandra.baga@gmail.com>
2022-09-15 10:30:50 +0200

    Image changed with React state

--- React/Meme_generator/components/Meme.jsx
+++ React/Meme_generator/components/Meme.jsx
@@ -1,9 +1,21 @@
 import React from "react"
+import memesData from "../src/data"
+
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+    
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -16,10 +28,12 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+            <img src={memeImage} className="meme--image" alt="MemeImage"/>
         </main>
     )
 }
