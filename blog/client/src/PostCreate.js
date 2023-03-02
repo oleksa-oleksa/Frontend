8a89a613786d205024ecf4d267f57498d5a77120
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-03-02 11:03:40 +0100

    Node.js course. React Blog App

---
+++ blog/client/src/PostCreate.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import axios from 'axios'
+
+export default () => {
+
+    const [title, setTitle] = useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post('http://localhost:4000/posts', {
+            title
+        });
+      setTitle('');  
+    };
+
+    return <div>
+        <form onSubmit={onSubmit}>
+            <div className="form-group"> 
+                <label>Title</label>
+                <input 
+                    values={title} 
+                    onChange={e => setTitle(e.target.value)} 
+                    className="form-control"></input>
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+};
