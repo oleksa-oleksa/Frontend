e7adb5e7d63b7d8761f579835e9cfa42a4c708be
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-03-02 11:30:56 +0100

    cors() added

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import PostCreate from "./PostCreate";
 
 const App = () => {
-    return <div className="container">
-        <h1>Create Post</h1>
-        <PostCreate />
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
     </div>
+  );
 };
-
 export default App;
--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -1,29 +1,34 @@
-import React, {useState} from "react";
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
-export default () => {
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
 
-    const [title, setTitle] = useState('');
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-    const onSubmit = async (event) => {
-        event.preventDefault();
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
 
-        await axios.post('http://localhost:4000/posts', {
-            title
-        });
-      setTitle('');  
-    };
+    setTitle("");
+  };
 
-    return <div>
-        <form onSubmit={onSubmit}>
-            <div className="form-group"> 
-                <label>Title</label>
-                <input 
-                    values={title} 
-                    onChange={e => setTitle(e.target.value)} 
-                    className="form-control"></input>
-            </div>
-            <button className="btn btn-primary">Submit</button>
-        </form>
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
+  );
 };
+
+export default PostCreate;
