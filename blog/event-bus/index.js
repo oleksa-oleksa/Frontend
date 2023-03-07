9bce6ca3e3adb7f885bf011ed5e4f301ece4db1d
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-03-07 14:25:34 +0100

    blog microservices created with react, express, axios

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
@@ -17,7 +21,14 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
