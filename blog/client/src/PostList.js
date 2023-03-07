b350114ec8f1c489a164f7702ef684bfefeb3504
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-03-07 10:47:04 +0100

    adding event bus in express

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
@@ -23,6 +25,8 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
