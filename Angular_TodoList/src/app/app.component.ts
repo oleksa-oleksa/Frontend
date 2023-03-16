e74ad069a155b42273db227548e63f28060a5790
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-03-16 12:36:19 +0100

    created Task class

--- Angular_TodoList/src/app/app.component.ts
+++ Angular_TodoList/src/app/app.component.ts
@@ -7,10 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_TodoList';
-  tasks = [
-    "Call Katja",
-    "Go to the Superfit Gym",
-    "Visit those awesome playground with a child afterworks",
-    "Try a new pizza from a small restaurant"
+  tasks: Task[] = [
+            new Task("Call Katja"),
+            new Task("Go to the Superfit Gym"),
+            new Task("Visit those awesome playground with a child afterworks"),
+            new Task("Try a new pizza from a small restaurant")
   ]
+
+  add(newTask: string){
+    //alert(newTask)
+    this.tasks.push(new Task(newTask))
+  }
+
+  remove(existingTask: Task) {
+    var userConfirmed = confirm(`Are you sure you want to remove: \n ${existingTask}`)
+
+    if (userConfirmed) {
+      this.tasks = this.tasks.filter(task => task != existingTask);
+    }
+  }
+
+  markAsDone(task: Task) {
+    //alert(`The task: "`+ task + '"is done')
+    task.isDone = true;
+  }
+}
+
+
+class Task {
+  constructor(public title: string) {
+
+  }
+  public isDone = false;
 }
