35032b9b97ccc86849f42b974d4e183bf6323adf
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-03-16 11:00:00 +0100

    dive into TypeScript

--- Angular_TodoList/src/app/app.component.ts
+++ Angular_TodoList/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_TodoList';
+  tasks = [
+    "Call Katja",
+    "Go to the Superfit Gym",
+    "Visit those awesome playground with a child afterworks",
+    "Try a new pizza from a small restaurant"
+  ]
 }
