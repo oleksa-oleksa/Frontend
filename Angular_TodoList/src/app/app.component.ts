8515b00f958dbfa28a52f948e1883262e97a7140
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-03-17 10:04:15 +0100

    added new component and code adjusted

--- Angular_TodoList/src/app/app.component.ts
+++ Angular_TodoList/src/app/app.component.ts
@@ -7,36 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_TodoList';
-  tasks: Task[] = [
-            new Task("Call Katja"),
-            new Task("Go to the Superfit Gym"),
-            new Task("Visit those awesome playground with a child afterworks"),
-            new Task("Try a new pizza from a small restaurant")
-  ]
-
-  add(newTask: string){
-    //alert(newTask)
-    this.tasks.push(new Task(newTask))
-  }
-
-  remove(existingTask: Task) {
-    var userConfirmed = confirm(`Are you sure you want to remove: \n ${existingTask}`)
-
-    if (userConfirmed) {
-      this.tasks = this.tasks.filter(task => task != existingTask);
-    }
-  }
-}
-
-
-class Task {
-  constructor(public title: string) {
-
-  }
-  public isDone = false;
-
-  toggleIsDone() {
-    //alert(`The task: "`+ task + '"is done')
-    this.isDone = !this.isDone;
-  }
 }
---
+++ Angular_TodoList/src/app/task-list/task-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit{
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  tasks: Task[] = [
+    new Task("Call Katja"),
+    new Task("Go to the Superfit Gym"),
+    new Task("Visit those awesome playground with a child afterworks"),
+    new Task("Try a new pizza from a small restaurant")
+  ]
+
+  add(newTask: string){
+    //alert(newTask)
+    this.tasks.push(new Task(newTask))
+  }
+
+  remove(existingTask: Task) {
+    var userConfirmed = confirm(`Are you sure you want to remove: \n ${existingTask.title}`)
+
+    if (userConfirmed) {
+    this.tasks = this.tasks.filter(task => task != existingTask);
+    }
+  }
+}
+
+
+class Task {
+  constructor(public title: string) {
+
+  }
+  public isDone = false;
+
+  toggleIsDone() {
+    //alert(`The task: "`+ task + '"is done')
+    this.isDone = !this.isDone;
+  }
+}
+
+
