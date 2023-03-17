cfd0f05b9004f1d8dad53a436e4acc0739216eb0
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-03-17 10:48:00 +0100

    added calendar and animation modules

--- Angular_TodoList/src/app/app.module.ts
+++ Angular_TodoList/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskListComponent } from './task-list/task-list.component';
+import { TaskListModule } from './task-list/task-list.module';
+import { CalendarModule } from './calendar/calendar.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TaskListModule,
+    CalendarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular_TodoList/src/app/calendar/calendar.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalendarComponent } from './calendar.component';
+
+
+
+@NgModule({
+  declarations: [
+    CalendarComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CalendarComponent
+  ]
+})
+export class CalendarModule { }
