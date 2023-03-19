a8b80c31fe5a8110576b4f1fe13c26f4524808eb
Oleksandra Baga <oleksandra.baga@gmail.com>
2023-03-19 13:40:07 +0100

    used ngModel instead of [value] to bind the value of the input field to the date property, and added a formControl to manage the form validation

--- Angular_TodoList/src/app/task-list/task-list.module.ts
+++ Angular_TodoList/src/app/task-list/task-list.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskListComponent } from './task-list.component';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     TaskListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDatepickerModule,
+    FormsModule
   ],
   exports: [
     TaskListComponent
